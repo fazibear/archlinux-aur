a53e4e3c313ddc02382602aa76196e49acd9cd23
Michał Kalbarczyk <fazibear@gmail.com>
2015-01-08 19:33:34 +0100

    add old pkgbuilds

---
+++ nbrubyide/nbrubyide.sh
@@ -0,0 +1,2 @@
+export NBRUBYIDE_HOME=/opt/nbrubyide
+export PATH=$PATH:$NBRUBYIDE_HOME/bin
---
+++ nbrubyide/refresh.sh
@@ -0,0 +1,31 @@
+# Contributor: FaziBear <fazibear@gmail.com>
+
+# makepkg configuration
+[ -f /etc/makepkg.conf ] && source /etc/makepkg.conf
+
+# main functions
+update_pb() {
+  sed -i "1,11 s/pkgver=$oldpkgver/pkgver=$1/" ./PKGBUILD
+}
+
+revert_pb() {
+  sed -i "1,11 s/pkgver=[^ ]*/pkgver=$oldpkgver/" ./PKGBUILD
+  exit 1
+}
+
+if [ -f ./PKGBUILD ]; then
+  source PKGBUILD || exit 1
+else
+  exit 1
+fi
+
+oldpkgver=$pkgver
+newpkgver=`wget -q -O - "$_hudson_url/job/$_hudson_job/lastSuccessfulBuild/buildNumber"`
+
+if [ "$oldpkgver" != "$newpkgver" ]; then
+  update_pb $newpkgver
+  makepkg || revert_pb
+  exit 0
+fi
+
+exit 0
